586e715f84ed6a819b51c477ea50eff5a5610508
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-04-12 15:48:12 -0400

    LibGen MySQL: rename volumes. Add date config

--- download/libgen/1.download.sh
+++ download/libgen/1.download.sh
@@ -2,17 +2,23 @@
 set -o errexit
 
 # Date of scimag backup
-DATE=2017-04-07
+DATE=`jq '.date' configuration.json`
+echo "$DATE"
 
 # Download LibGen science magazine metadata from libgen.io
 wget --timestamping \
-  --directory-prefix=mysql \
+  --directory-prefix=sql \
   http://libgen.io/dbdumps/backup_archive/scimag_dbbackup-$DATE.rar
 
 # Extract from the rar
 # WARNING: unrar is non-free software
-unrar -ierr p mysql/scimag_dbbackup-$DATE.rar backup_scimag.sql \
-  > mysql/backup_scimag_$DATE.sql
+unrar -ierr p sql/scimag_dbbackup-$DATE.rar backup_scimag.sql \
+  > sql/backup_scimag_$DATE.sql
 
-# Track SQL with Git LFS using a highly-compressed and free format
-xz --keep mysql/backup_scimag_$DATE.sql
+# Compress SQL file using a free format
+# Use extreme 9 to reduce file size as much as possible
+xz \
+  --extreme -9 \
+  --threads=0 \
+  --keep \
+  sql/backup_scimag_$DATE.sql
