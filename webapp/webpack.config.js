e5ba13021435c8b93ff680e0f23c51c19bdc746e
Ariel Rodriguez <arielsvn@gmail.com>
2017-06-10 17:34:49 -0400

    fix build configuration to load svg

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -82,7 +82,6 @@ module.exports = {
       { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" },
     ]
   },
 
