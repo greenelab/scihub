572d6947cb958e797d1a07fdb273157ad9154273
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-07-27 14:41:49 -0400

    Webapp: update publisher table data version
    Refs https://github.com/greenelab/scihub-manuscript/issues/15#issuecomment-318301167

--- webapp/env.js
+++ webapp/env.js
@@ -3,6 +3,6 @@
 
 module.exports = {
   journals_data: 'https://raw.githubusercontent.com/greenelab/scihub/75ffe6e1422b0b6a9a0034f6e671dd34601327cd/data/journal-coverage.tsv',
-  publishers_data: 'https://raw.githubusercontent.com/greenelab/scihub/1838a517f242020d79481201d62943bfb04d5876/data/coverage-by-category.tsv',
+  publishers_data: 'https://raw.githubusercontent.com/greenelab/scihub/39ac7f29cc1a2d0465dcc36a07d3854242f95738/data/coverage-by-category.tsv',
 };
 
