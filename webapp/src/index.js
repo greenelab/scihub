f8ca4f875726eef8de6420b97987330107d32bb5
Ariel <arielsvn@gmail.com>
2019-06-02 21:05:15 -0400

    Add bootstrap-sass package and include it in styles

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root';
 
-import './theme/bootstrap.scss';
+import './theme/main.scss';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
